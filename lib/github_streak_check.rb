f851322b8d82fe0e90de4e328d393855b01080ae
Yutaka HARA <yutaka.hara+github@gmail.com>
2013-11-06 02:20:22 +0900

    Send a mail with -m

--- lib/github_streak_check.rb
+++ lib/github_streak_check.rb
@@ -5,8 +5,8 @@ require 'date'
 require 'pony'
 
 class GithubStreakCheck
-  def initialize(login)
-    @login = login
+  def initialize(opts)
+    @opts = opts
   end
 
   def run
@@ -14,13 +14,21 @@ class GithubStreakCheck
       puts "Check OK: you've already done today's contribution."
       exit 0
     else
-      $stderr.puts "Check failed: You have not extended today's streak yet"
+      msg = "Check failed: You have not extended today's streak yet"
+      $stderr.puts msg
+      if (addr = @opts[:mail_to])
+        ret = Pony.mail(to: addr,
+                        from: addr,
+                        subject: "GithubStreakCheck",
+                        body: msg)
+        puts "Sent mail: #{ret.inspect}"
+      end
       exit 1
     end
   end
 
   def commited_today?
-    json = JSON.parse(open("https://api.github.com/users/#{@login}/events?per_page=1").read)
+    json = JSON.parse(open("https://api.github.com/users/#{@opts[:username]}/events?per_page=1").read)
     return false if json.length == 0
 
     event = json.first
--- lib/github_streak_check/version.rb
+++ lib/github_streak_check/version.rb
@@ -1,3 +1,3 @@
 class GithubStreakCheck
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
